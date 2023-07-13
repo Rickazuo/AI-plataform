b3f2c43ac0ffd7cba5201f00a9d9290015a2defb
Rickazuo <rick3209@gmail.com>
2023-07-13 19:34:08 -0300

    update components and main

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
+import Header from './components/Header/Header'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div>
-
+      <div className={styles.box}>
+        <Header />
       </div>
     </main>
   )
